f97e2eb86b94ad8118d9863adc1a1c0a02af4b17
ipipman <ipipman@163.com>
2021-04-13 14:37:03 +0800

    add spring frame in gof example

---
+++ GOF/gof-example/src/main/java/com/ipipman/gof/example/GofExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipipman.gof.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofExampleApplication.class, args);
+	}
+
+}
---
+++ GOF/gof-example/src/test/java/com/ipipman/gof/example/GofExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipipman.gof.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
